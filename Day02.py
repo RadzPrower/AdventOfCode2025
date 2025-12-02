30cead7c6036aa928f84b374f7674ea5b7cb6924
RadzPrower <anthony.johns.6@gmail.com>
2025-12-02 10:53:34 -0500

    Implementation of Day 2 solution
    Signed-off-by: RadzPrower <anthony.johns.6@gmail.com>

--- Day02.py
+++ Day02.py
@@ -1,3 +1,20 @@
 def main(data):
-    print("This day is not yet implemented.")
-    return
+    return find_invalids(data)
+
+def find_invalids(data):
+    invalid_id_sum = 0
+    ranges = data[0].split(',')
+    for id_range in ranges:
+        start, end = id_range.split('-')
+        for i in range(int(start), int(end)+1):
+            i_string = str(i)
+            final_segment_length = len(i_string)//2
+            for segment_length in range(1, final_segment_length + 1):
+                segments = []
+                for j in range(0, len(i_string), segment_length):
+                    segments.append(i_string[j:j + segment_length])
+                if len(segments) > 1 and len(set(segments)) == 1:
+                    invalid_id_sum += i
+                    break
+    result = "The sum of the invalid IDs is " + str(invalid_id_sum) + ".\n"
+    return result
