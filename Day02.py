f5b0ab9aff8ef2a0b390f55cc99408607d9bd314
RadzPrower <anthony.johns.6@gmail.com>
2025-12-02 11:15:50 -0500

    Optimization of Day 2 solution
    Signed-off-by: RadzPrower <anthony.johns.6@gmail.com>

--- Day02.py
+++ Day02.py
@@ -8,13 +8,15 @@ def find_invalids(data):
         start, end = id_range.split('-')
         for i in range(int(start), int(end)+1):
             i_string = str(i)
-            final_segment_length = len(i_string)//2
+            length = len(i_string)
+            final_segment_length = length//2
             for segment_length in range(1, final_segment_length + 1):
-                segments = []
-                for j in range(0, len(i_string), segment_length):
-                    segments.append(i_string[j:j + segment_length])
-                if len(segments) > 1 and len(set(segments)) == 1:
-                    invalid_id_sum += i
-                    break
+                if length % segment_length == 0:
+                    segments = []
+                    for j in range(0, length, segment_length):
+                        segments.append(i_string[j:j + segment_length])
+                    if len(segments) > 1 and len(set(segments)) == 1:
+                        invalid_id_sum += i
+                        break
     result = "The sum of the invalid IDs is " + str(invalid_id_sum) + ".\n"
     return result
