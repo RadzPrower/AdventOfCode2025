9b529dda3b87cce8340ae8ccfc75ed502a27a501
RadzPrower <anthony.johns.6@gmail.com>
2025-12-12 09:37:59 -0500

    Implementation of Day 12 Solution
    Signed-off-by: RadzPrower <anthony.johns.6@gmail.com>

--- Day12.py
+++ Day12.py
@@ -1,3 +1,20 @@
 def main(data):
-    print("This day is not yet implemented.")
-    return
+    results = 0
+    regions, presents = parse_data(data)
+    for i, region in enumerate(regions):
+        region_area = region[0] * region[1]
+        presents_area = sum(presents[i]) * 9
+        if presents_area <= region_area:
+            results += 1
+    return f'{results} regions can fit their listed presents.'
+
+def parse_data(data):
+    region_list = []
+    presents_list = []
+    for i in range(30,len(data)):
+        region, presents = data[i].split(': ')
+        region = tuple(map(int, region.split('x')))
+        presents = tuple(map(int, presents.split()))
+        region_list.append(region)
+        presents_list.append(presents)
+    return region_list, presents_list
